b6f3d38d7328b194912d9ff94642b5a2224bd0b4
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2020-05-10 22:55:34 +0300

    исправить можно лучше

--- script/popup.js
+++ script/popup.js
@@ -8,30 +8,21 @@ const popupInputName = page.querySelector(".popup__input_type_name");
 const popupInputVocation = page.querySelector(".popup__input_type_vocation");
 const popupContainer = popup.querySelector(".popup__container");
 const cardGridPlace = page.querySelectorAll(".card-grid__place");
-function openPopup() { //функция открытия формы изменения данных
-  popupInputName.value = profileName.textContent;
-  popupInputVocation.value = profileVocation.textContent;
-  popup.classList.add('popup_opened');
-}
-function closePopup() { //функция закрытия формы изменения данных
-  popup.classList.remove('popup_opened');
+function openClosePopup() { //функция открытия/закрытия формы изменения данных
+  if(popup.classList.contains('popup_opened')){
+    popup.classList.remove('popup_opened');
+  }else{
+    popupInputName.value = profileName.textContent;
+    popupInputVocation.value = profileVocation.textContent;
+    popup.classList.add('popup_opened');
+  }   
 }
 function formSubmitHandler(evt) { //функция созхранения данных и закрытия формы изменения данных
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileVocation.textContent = popupInputVocation.value;
-  popup.classList.remove('popup_opened');
+  openClosePopup();
 }
-function resizeFuction() {
-
-  if (page.clientWidth < 500) {
-    cardGridPlace[0].textContent = 'Карачаево-Ч...';
-  } else {
-    cardGridPlace[0].textContent = 'Карачаевск';
-  }
-}
-resizeFuction();
-window.onresize = resizeFuction;
-profileEditButton.addEventListener('click', openPopup);  //прерывание на нажатие кнопки изменения данных
-popupCancelButton.addEventListener('click', closePopup); //прерывание на нажатие кнопки закрытия формы изменения данных
+profileEditButton.addEventListener('click', openClosePopup);  //прерывание на нажатие кнопки изменения данных
+popupCancelButton.addEventListener('click', openClosePopup); //прерывание на нажатие кнопки закрытия формы изменения данных
 popupContainer.addEventListener('submit', formSubmitHandler);
