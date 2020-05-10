b5c80a52466a20d61924209b7e552359d61ccab6
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2020-05-10 21:33:17 +0300

    написать комментарии

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let page = document.querySelector(".page");
+const page = document.querySelector(".page");
 const profileEditButton = page.querySelector(".profile__edit-button");
 const popupCancelButton = page.querySelector(".popup__cancel-button");
-let popup = page.querySelector(".popup");
-let profileName = page.querySelector(".profile__name");
-let profileVocation = page.querySelector(".profile__vocation");
-let popupInputName = page.querySelector(".popup__input_type_name");
-let popupInputVocation = page.querySelector(".popup__input_type_vocation");
-let popupContainer = popup.querySelector(".popup__container");
-let cardGridPlace = page.querySelectorAll(".card-grid__place");
-function openPopup() {
+const popup = page.querySelector(".popup");
+const profileName = page.querySelector(".profile__name");
+const profileVocation = page.querySelector(".profile__vocation");
+const popupInputName = page.querySelector(".popup__input_type_name");
+const popupInputVocation = page.querySelector(".popup__input_type_vocation");
+const popupContainer = popup.querySelector(".popup__container");
+const cardGridPlace = page.querySelectorAll(".card-grid__place");
+function openPopup() { //функция открытия формы изменения данных
   popupInputName.value = profileName.textContent;
   popupInputVocation.value = profileVocation.textContent;
   popup.classList.add('popup_opened');
 }
-function closePopup() {
+function closePopup() { //функция закрытия формы изменения данных
   popup.classList.remove('popup_opened');
 }
-function formSubmitHandler(evt) {
+function formSubmitHandler(evt) { //функция созхранения данных и закрытия формы изменения данных
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileVocation.textContent = popupInputVocation.value;
@@ -32,6 +32,6 @@ function resizeFuction() {
 }
 resizeFuction();
 window.onresize = resizeFuction;
-profileEditButton.addEventListener('click', openPopup);
-popupCancelButton.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);  //прерывание на нажатие кнопки изменения данных
+popupCancelButton.addEventListener('click', closePopup); //прерывание на нажатие кнопки закрытия формы изменения данных
 popupContainer.addEventListener('submit', formSubmitHandler);
