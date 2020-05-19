beb9f544037ee159f543108a637ae46d88959017
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2020-05-19 13:23:30 +0300

    реализовать окрытие popup с картинкой

--- script/jsscript.js
+++ script/jsscript.js
@@ -10,65 +10,85 @@ const popupContainer = popup.querySelector(".popup__container");
 const cardGrid = page.querySelector(".card-grid");
 const cardTemplate = document.querySelector("#card").content;
 const profileAddButton = page.querySelector(".profile__add-button");
+const imagePopup = page.querySelector(".image-popup");
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-function addCard(name, link){ //Функция добавления карточки
+
+function openCloseImagePopup() {//функция открытия/закрытия popup с картинкой
+  imagePopup.classList.toggle('image-popup_opened');
+
+}
+
+imagePopup.querySelector(".image-popup__cancel-button").addEventListener('click', openCloseImagePopup); //прерывание при нажатии кнопки закрытия popup с картинкой
+
+function addCard(name, link) { //Функция добавления карточки
   const cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector(".card-grid__place").textContent = name;
-  cardElement.querySelector(".card-grid__image").src = link; 
+  const cardGridImage = cardElement.querySelector(".card-grid__image");
+  cardGridImage.src = link;
+  cardGridImage.addEventListener('click', function () {//Прерывание на нажатие картинки
+    const card = cardGridImage.closest(".card-grid__item");
+    imagePopup.querySelector(".image-popup__image").src = card.querySelector(".card-grid__image").src;
+    imagePopup.querySelector(".image-popup__name").textContent = card.querySelector(".card-grid__place").textContent;
+    openCloseImagePopup();
+  });
   const cardGridDeleteButton = cardElement.querySelector(".card-grid__delete-button");
-  cardGridDeleteButton.addEventListener('click', function () {
+  cardGridDeleteButton.addEventListener('click', function () {//прерывание на нажатие кнопки удаления карточки
     cardGridDeleteButton.closest(".card-grid__item").remove();
-  });//прерывание на нажатие кнопки удаления карточки
-  
+  });
+
   const cardGridLikeButton = cardElement.querySelector(".card-grid__like");
-  cardGridLikeButton.addEventListener('click',function(){
+  cardGridLikeButton.addEventListener('click', function () {//прерывание на нажатие кнопки лайк
     cardGridLikeButton.classList.toggle('card-grid__like_active');
   });
   cardGrid.append(cardElement);
 }
+
 initialCards.forEach(function (item) {//Добавление карточек из массива
   addCard(item.name, item.link);
 });
+
 function openClosePopup() { //функция открытия/закрытия формы изменения данных
-  if(popup.classList.contains('popup_opened')){
+  if (popup.classList.contains('popup_opened')) {
     popup.classList.remove('popup_opened');
-  }else{
+  } else {
     popupInputName.value = profileName.textContent;
     popupInputVocation.value = profileVocation.textContent;
     popup.classList.add('popup_opened');
-  }   
+  }
 }
+
 function formSubmitHandler(evt) { //функция созхранения данных и закрытия формы изменения данных
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileVocation.textContent = popupInputVocation.value;
   openClosePopup();
 }
+
 profileEditButton.addEventListener('click', openClosePopup);  //прерывание на нажатие кнопки изменения данных
 popupCancelButton.addEventListener('click', openClosePopup); //прерывание на нажатие кнопки закрытия формы изменения данных
 popupContainer.addEventListener('submit', formSubmitHandler);//Прерывание на нажатие кнопки сохранить
