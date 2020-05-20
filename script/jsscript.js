7cb316445c267ad0d76305ee805334d8e84f2d05
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2020-05-20 11:51:46 +0300

    реализовать плавное открытие/закрытие popup

--- script/jsscript.js
+++ script/jsscript.js
@@ -43,12 +43,55 @@ const initialCards = [
   }
 ];
 
-function openCloseImagePopup() {//функция открытия/закрытия popup с картинкой
-  imagePopup.classList.toggle('image-popup_opened');
-
+function removeVisiblePopup() {// функция удаления 'popup_visible' из popup
+  popup.classList.remove('popup_visible')
 }
 
-imagePopup.querySelector(".image-popup__cancel-button").addEventListener('click', openCloseImagePopup); //прерывание при нажатии кнопки закрытия popup с картинкой
+function removeVisibleImagePopup() {// функция удаления 'popup_visible' из imagePopup
+  imagePopup.classList.remove('popup_visible')
+}
+
+function removeVisibleAddPopup() {// функция удаления 'popup_visible' из addPopup
+  addPopup.classList.remove('popup_visible')
+}
+
+function openCloseImagePopup() {//функция открытия/закрытия формы добавления карточки
+
+  if (imagePopup.classList.contains('popup_opened')) {
+    imagePopup.classList.remove('popup_opened');
+    setTimeout(removeVisibleImagePopup, 500);
+  } else {
+
+    imagePopup.classList.add('popup_opened');
+    imagePopup.classList.add('popup_visible');
+  }
+}
+
+function openClosePopup() { //функция открытия/закрытия формы изменения данных
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+    setTimeout(removeVisiblePopup, 500);
+  } else {
+    popupInputName.value = profileName.textContent;
+    popupInputVocation.value = profileVocation.textContent;
+    popup.classList.add('popup_opened');
+    popup.classList.add('popup_visible');
+  }
+}
+
+function openCloseAddPopup() {//функция открытия/закрытия формы добавления карточки
+
+  addPopupInputName.value = null;
+  addPopupInputLink.value = null;
+  if (addPopup.classList.contains('popup_opened')) {
+    addPopup.classList.remove('popup_opened');
+    setTimeout(removeVisibleAddPopup, 500);
+  } else {
+
+    addPopup.classList.add('popup_opened');
+    addPopup.classList.add('popup_visible');
+  }
+}
 
 function addCard(name, link) { //Функция добавления карточки
   const cardElement = cardTemplate.cloneNode(true);
@@ -77,32 +120,20 @@ initialCards.forEach(function (item) {//Добавление карточек и
   addCard(item.name, item.link);
 });
 
-function openClosePopup() { //функция открытия/закрытия формы изменения данных
-  if (popup.classList.contains('popup_opened')) {
-    popup.classList.remove('popup_opened');
-  } else {
-    popupInputName.value = profileName.textContent;
-    popupInputVocation.value = profileVocation.textContent;
-    popup.classList.add('popup_opened');
-  }
-}
-function openCloseAddPopup(){//функция открытия/закрытия формы добавления карточки
-  
-  addPopupInputName.value = null;
-  addPopupInputLink.value = null;
-  addPopup.classList.toggle('add-popup_opened');
-}
 function formAddHandler(evt) { //функция добавления карты и закрытия формы 
   evt.preventDefault();
   addCard(addPopupInputName.value, addPopupInputLink.value);
   openCloseAddPopup();
 }
+
 function formSubmitHandler(evt) { //функция созхранения данных и закрытия формы изменения данных
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileVocation.textContent = popupInputVocation.value;
   openClosePopup();
 }
+
+imagePopup.querySelector(".image-popup__cancel-button").addEventListener('click', openCloseImagePopup); //прерывание при нажатии кнопки закрытия popup с картинкой
 profileAddButton.addEventListener('click', openCloseAddPopup);//Прерывание на нажатие кнопки добавить
 addPopupCancelButton.addEventListener('click', openCloseAddPopup);//Прерывание на нажатие кнопки закрыть
 addPopupContainer.addEventListener('submit', formAddHandler);//Прерывание на нажатие кнопки сохранить
