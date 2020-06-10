1f81427c531584c100c0de6a0a72c9f6abb4d7ed
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2020-06-10 21:50:22 +0300

    исправить мелочи

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const page = document.querySelector(".page");
 const profileEditButton = page.querySelector(".profile__edit-button");
 const popupCancelButton = page.querySelector(".popup__cancel-button");
-const popup = page.querySelector(".popup");
+const popup = page.querySelector(".edit-popup");
 const profileName = page.querySelector(".profile__name");
 const profileVocation = page.querySelector(".profile__vocation");
 const popupInputName = popup.querySelector("#name-input");
@@ -18,6 +18,7 @@ const addPopupContainer = addPopup.querySelector(".popup__container");
 const addPopupInputName = addPopup.querySelector("#title-input");
 const addPopupInputLink = addPopup.querySelector("#link-input");
 const addPopupCancelButton = addPopup.querySelector(".popup__cancel-button");
+const formsList = document.querySelectorAll(".popup__container");
 const initialCards = [
   {
     name: 'Архыз',
@@ -44,18 +45,18 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-function openPopup(input) {
+function openPopup(input) {//функция открытия всплывающего окна
   input.classList.add('popup_opened');
   addEventListener('keydown', eventKeyHandler);
   input.addEventListener('click', overlayClickHandler);
 
 }
-function closePopup(input) {
+function closePopup(input) {//функция закрытия всплывающего окна
   input.classList.remove('popup_opened');
   removeEventListener('keydown', eventKeyHandler);
   input.removeEventListener('click', overlayClickHandler);
 }
-function imageClickHandler(name, link) {
+function imageClickHandler(name, link) {// обработчик клика по картинке
   imagePopupImage.src = link;
   imagePopupName.textContent = name;
   imagePopupImage.setAttribute('alt', `Фото '${name}'`);
@@ -100,47 +101,34 @@ function formSubmitHandler(evt) { //функция сохранения данн
   profileVocation.textContent = popupInputVocation.value;
   closePopup(popup);
 }
-const eventKeyHandler = (evt) => {
+const eventKeyHandler = (evt) => {//обработчик нажатия клавиши
   const openedPopup = document.querySelector('.popup_opened');
   if (evt.key === "Escape") closePopup(openedPopup);
 };
-const overlayClickHandler = (evt) => {
+const overlayClickHandler = (evt) => {//обработчик клика
   closePopup(evt.target);
 };
-function openProfileForm() {
+function openProfileForm() {//функция открытия формы изменения данных
   popupInputName.value = profileName.textContent;
   popupInputVocation.value = profileVocation.textContent;
   openPopup(popup);
 }
 
-imagePopup.querySelector(".popup__cancel-button").addEventListener('click', function () {
-  closePopup(imagePopup);
-}); //прерывание при нажатии кнопки закрытия popup с картинкой
-profileAddButton.addEventListener('click', function () {
-  openPopup(addPopup);
-}); //Прерывание на нажатие кнопки добавить
-addPopupCancelButton.addEventListener('click', function () {
-  closePopup(addPopup);
-});//Прерывание на нажатие кнопки закрыть
+imagePopup.querySelector(".popup__cancel-button").addEventListener('click', () => closePopup(imagePopup)); //прерывание при нажатии кнопки закрытия popup с картинкой
+profileAddButton.addEventListener('click', () => openPopup(addPopup)); //Прерывание на нажатие кнопки добавить
+addPopupCancelButton.addEventListener('click', () => closePopup(addPopup));//Прерывание на нажатие кнопки закрыть
 addPopupContainer.addEventListener('submit', formAddHandler);//Прерывание на нажатие кнопки сохранить
 profileEditButton.addEventListener('click', openProfileForm);  //прерывание на нажатие кнопки изменения данных
-popupCancelButton.addEventListener('click', function () {
-  closePopup(popup);
-}); //прерывание на нажатие кнопки закрытия формы изменения данных
+popupCancelButton.addEventListener('click',() => closePopup(popup)); //прерывание на нажатие кнопки закрытия формы изменения данных
 popupContainer.addEventListener('submit', formSubmitHandler);//Прерывание на нажатие кнопки сохранит
-enableValidation({
-  formSelector: '#edit-form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__apply-button',
-  inactiveButtonClass: 'popup__apply-button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
-enableValidation({
-  formSelector: '#add-form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__apply-button',
-  inactiveButtonClass: 'popup__apply-button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
+
+formsList.forEach((inputElement) => {
+  enableValidation({
+    formSelector: `#${inputElement.id}`,
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__apply-button',
+    inactiveButtonClass: 'popup__apply-button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
+  });
