032b207ad2d8685f3b14cb2d8c63375669e62c69
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2020-06-23 12:08:18 +0300

    поправить скрипт класса Card

--- script/Card.js
+++ script/Card.js
@@ -1,34 +1,41 @@
 import { openPopup, imagePopup, imagePopupImage, imagePopupName } from './index.js'
 export class Card {
   constructor(data, templateElementSelector){
-    this.templateElement = templateElementSelector;
-    this.name = data.name;
-    this.link = data.link;
+    this._templateElement = templateElementSelector;
+    this._name = data.name;
+    this._link = data.link;
+    this._cardElement = 1;
   }
   _imageClickHandler() {// обработчик клика по картирке
-    imagePopupImage.src = this.link;
-    imagePopupName.textContent = this.name;
-    imagePopupImage.setAttribute('alt', `Р¤РѕС‚Рѕ '${this.name}'`);
+    imagePopupImage.src = this._link;
+    imagePopupName.textContent = this._name;
+    imagePopupImage.setAttribute('alt', `Фото '${this._name}'`);
     openPopup(imagePopup);
   }
-  addCard() { //функция добавления карточки
-    const cardTemplate = document.querySelector(this.templateElement).content;
+  _deleteClickHandler(){//обработчик нажатия кнопки удаления
+    this.closest(".card-grid__item").remove();
+  }
+  _likeClickHandler(){//обработчик нажатия кнопки лайк
+    this.classList.toggle('card-grid__like_active');
+  }
+  _getCardElement(){//функция получения шаблона карточки
+    const cardTemplate = document.querySelector(this._templateElement).content;
     const cardElement = cardTemplate.cloneNode(true);
-    const cardGridPlace = cardElement.querySelector(".card-grid__place");
-    cardGridPlace.textContent = this.name;
-    const cardGridImage = cardElement.querySelector(".card-grid__image");
-    cardGridImage.src = this.link;
-    cardGridImage.setAttribute('alt', `Р¤РѕС‚Рѕ '${this.name}'`);
-    cardGridImage.addEventListener('click', () => this._imageClickHandler());// прерывание на клик по картинке
-    const cardGridDeleteButton = cardElement.querySelector(".card-grid__delete-button");
-    cardGridDeleteButton.addEventListener('click', function () {//прерывание на клик по кнопке удаления 
-      cardGridDeleteButton.closest(".card-grid__item").remove();
-    });
-    const cardGridLikeButton = cardElement.querySelector(".card-grid__like");
-    cardGridLikeButton.addEventListener('click', function () {//прерывание на клик по кнопке лайк
-      cardGridLikeButton.classList.toggle('card-grid__like_active');
-    });
     return cardElement;
+  }
+  _enabledListerners(cardGridImage,cardGridDeleteButton,cardGridLikeButton){//функция включения слушателей
+    cardGridImage.addEventListener('click', () => this._imageClickHandler());// прерывание на клик по картинке
+    cardGridDeleteButton.addEventListener('click', this._deleteClickHandler);//прерывание на клик по кнопке удаления 
+    cardGridLikeButton.addEventListener('click', this._likeClickHandler);//прерывание на клик по кнопке лайк
+  }
+  addCard() { //функция добавления карточки
+    this._cardElement = this._getCardElement();
+    this._cardElement.querySelector(".card-grid__place").textContent = this._name;
+    const cardGridImage = this._cardElement.querySelector(".card-grid__image");
+    cardGridImage.src = this._link;
+    cardGridImage.setAttribute('alt', `Фото '${this._name}'`);
+    this._enabledListerners(cardGridImage, this._cardElement.querySelector(".card-grid__delete-button"), this._cardElement.querySelector(".card-grid__like"));
+    return this._cardElement;
   }
 
 }
